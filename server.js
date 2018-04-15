9ebcaaae775d1c7f9b2e3003a8722c566ce2736c
Hoppy <mark82hopcraft@gmail.com>
2018-04-15 18:49:11 +0100

    test

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 
+const path = require('path')
 const express = require('express')
+const pug = require('pug')
+const bodyParser = require('body-parser')
 const PORT = 3000
+const ROUTESINDEX = require(path.join(__dirname, '/server/routes/index'))
 
-var server = express();
+var server = express()
 
-server.listen(PORT);
-console.log(`Server started on port: ${PORT}`);
+server.set('view engine', 'pug')
+server.set('views', path.join(__dirname, 'client/views'))
+server.use(express.static(path.join(__dirname, 'client/static')))
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: false}))
+
+
+
+server.listen(PORT)
+console.log(`Server started on port: ${PORT}`)
