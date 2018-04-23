ae6c7d22191f5d4ef1f41cf1a218cc2cdc7ebd28
Hoppy <mark82hopcraft@gmail.com>
2018-04-23 17:48:05 +0100

    started nav carousel layout

--- client/static/source-js/home.js
+++ client/static/source-js/home.js
@@ -1,3 +1,4 @@
+let body = document.getElementsByTagName('body')[0]
 let canvas1 = document.getElementsByTagName('canvas')[0]
 let ctx1 = canvas1.getContext('2d')
 
@@ -8,48 +9,48 @@ let CANVAS1_RIGHT
 
 /*----------------------------------------------------------------BREAK POINTS*/
 function setLayout() {
-  console.log(`window.innerWidth: ${window.innerWidth}, window.innerHeight: ${window.innerHeight}`)
+  console.log(`body.clientWidth: ${body.clientWidth}, body.clientHeight: ${body.clientHeight}`)
 
   //-------------------------------------------------------------SMALL SCREENS
   //small width in portrait
-  if (window.innerHeight > window.innerWidth && window.innerWidth <= 480) {
+  if (body.clientHeight > body.clientWidth && body.clientWidth <= 480) {
     console.log('SCREEN: small width in portrait')
-    CANVAS1_RIGHT = window.innerWidth
-    CANVAS1_BOTTOM = window.innerHeight * 0.5
+    CANVAS1_RIGHT = body.clientWidth
+    CANVAS1_BOTTOM = body.clientHeight * 0.5
   }
   //small height in landscape
-  if (window.innerHeight < window.innerWidth && window.innerHeight <= 480) {
+  if (body.clientHeight < body.clientWidth && body.clientHeight <= 480) {
     console.log('SCREEN: small height in landscape')
-    CANVAS1_RIGHT = window.innerWidth * 0.5
-    CANVAS1_BOTTOM = window.innerHeight
+    CANVAS1_RIGHT = body.clientWidth * 0.5
+    CANVAS1_BOTTOM = body.clientHeight
   }
 
   //------------------------------------------------------------MEDIUM SCREENS
   //medium width in portrait
-  if (window.innerHeight > window.innerWidth && window.innerWidth <= 1024 && window.innerWidth > 480) {
+  if (body.clientHeight > body.clientWidth && body.clientWidth <= 1024 && body.clientWidth > 480) {
     console.log('SCREEN: medium width in portrait')
-    CANVAS1_RIGHT = window.innerWidth
-    CANVAS1_BOTTOM = window.innerHeight * 0.7
+    CANVAS1_RIGHT = body.clientWidth
+    CANVAS1_BOTTOM = body.clientHeight * 0.7
   }
   //medium height in landscape
-  if (window.innerHeight < window.innerWidth && window.innerHeight <= 1024 && window.innerHeight > 480) {
+  if (body.clientHeight < body.clientWidth && body.clientHeight <= 1024 && body.clientHeight > 480) {
     console.log('SCREEN: medium height in landscape')
-    CANVAS1_RIGHT = window.innerWidth * 0.65
-    CANVAS1_BOTTOM = window.innerHeight
+    CANVAS1_RIGHT = body.clientWidth * 0.65
+    CANVAS1_BOTTOM = body.clientHeight
   }
 
   //------------------------------------------------------------LARGE SCREENS
-  //large widtht in portrait
-  if (window.innerHeight > window.innerWidth && window.innerWidth > 1024) {
+  //large width in portrait
+  if (body.clientHeight > body.clientWidth && body.clientWidth > 1024) {
     console.log('SCREEN: large width in portrait')
-    CANVAS1_RIGHT = window.innerWidth
-    CANVAS1_BOTTOM = window.innerHeight * 0.65
+    CANVAS1_RIGHT = body.clientWidth
+    CANVAS1_BOTTOM = body.clientHeight * 0.65
   }
   //large height in landscape
-  if (window.innerHeight < window.innerWidth && window.innerHeight > 1024) {
+  if (body.clientHeight < body.clientWidth && body.clientHeight > 1024) {
     console.log('SCREEN: large height in landscape')
-    CANVAS1_RIGHT = window.innerWidth * 0.65
-    CANVAS1_BOTTOM = window.innerHeight
+    CANVAS1_RIGHT = body.clientWidth * 0.65
+    CANVAS1_BOTTOM = body.clientHeight
   }
 
   canvas1.width = CANVAS1_RIGHT
@@ -65,7 +66,7 @@ function drawTempShape() {
 
   console.log('drawing...')
   ctx1.strokeStyle = 'white'
-  ctx1.lineWidth = 8
+  ctx1.lineWidth = 4
   ctx1.beginPath()
   let x = CANVAS1_RIGHT / 2
   let y = CANVAS1_BOTTOM / 2
